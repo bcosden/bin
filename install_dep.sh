e57fb1cbc69ef2b43809898370ed7bbf29f4f8d8
Bruce M. Cosden <bruce.cosden@gmail.com>
2022-06-25 08:27:58 -0400

    updated

---
+++ install_dep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install python pip
+sudo apt-get install python3-pip
+
+# install csvkit for csvlook tool
+sudo pip install csvkit
+
+# install jq for JSON formatting
+sudo apt-get install jq
+
